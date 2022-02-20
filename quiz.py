e6099b49df66d287293964d7fd581c5abf3bb049
Nitin Kumar Sharma <85667515+NitinSharma900@users.noreply.github.com>
2022-02-20 22:02:32 +0530

    Add files via upload

---
+++ quiz.py
@@ -0,0 +1,527 @@
+import tkinter as tk
+from tkinter import *
+import random
+import time
+from PIL import Image,ImageTk
+
+def menu():
+    root.destroy()
+    global menu 
+    menu = Tk()
+    
+    
+    menu_canvas = Canvas(menu,width=720,height=440,bg="#101357")
+    menu_canvas.pack()
+
+    menu_frame = Frame(menu_canvas,bg="white")
+    menu_frame.place(relwidth=0.8,relheight=0.8,relx=0.1,rely=0.1)
+
+    
+    
+    wel = Label(menu_canvas,text=' W E L C O M E  T O  Q U I Z  S T A T I O N ',fg="white",bg="#101357") 
+    wel.config(font=('Broadway 22'))
+    wel.place(relx=0.1,rely=0.02)
+    
+    
+    level = Label(menu_frame,text='Select your Difficulty Level !!',bg="white",font="verdana 18")
+    level.place(relx=0.25,rely=0.3)
+    
+    Button(menu_frame,text='Easy',command=easy,font="verdana").place(x=250,y=200,width=100)
+    Button(menu_frame,text='Medium',command=medium,font="verdana").place(x=250,y=250,width=100)
+    Button(menu_frame,text='Hard',command=difficult,font="verdana").place(x=250,y=300,width=100)
+    
+    def navigate():
+        
+        x = var.get()
+        print(x)
+        if x == 1:
+            menu.destroy()
+            easy()
+        elif x == 2:
+            menu.destroy()
+            medium()
+        
+        elif x == 3:
+            menu.destroy()
+            difficult()
+        else:
+            pass
+
+    menu.mainloop()
+def easy():
+    menu.destroy()
+    global e
+    e = Tk()
+    
+    easy_canvas = Canvas(e,width=720,height=440,bg="#101357")
+    easy_canvas.pack()
+
+    easy_frame = Frame(easy_canvas,bg="white")
+    easy_frame.place(relwidth=0.8,relheight=0.8,relx=0.1,rely=0.1)
+
+    
+    def countDown():
+        check = 0
+        for k in range(10, 0, -1):
+            
+            if k == 1:
+                check=-1
+            timer.configure(text=k)
+            easy_frame.update()
+            time.sleep(1)
+            
+        timer.configure(text="Times up!")
+        if check==-1:
+            return (-1)
+        else:
+            return 0
+    global score
+    score = 0
+    
+    easyQ = [
+                 [
+                     "What will be the output of the following Python code? \nl=[1, 0, 2, 0, 'hello', '', []] \nlist(filter(bool, nl))",
+                     "[1, 0, 2, ‘hello’, '', []]",
+                     "Error",
+                     "[1, 2, ‘hello’]",
+                     "[1, 0, 2, 0, ‘hello’, '', []]" 
+                 ] ,
+                 [
+                     "What will be the output of the following Python expression if the value of x is 34? \nprint(“%f”%x)" ,
+                    "34.00",
+                    "34.000000",
+                    "34.0000",
+                    "34.00000000"
+                     
+                 ],
+                [
+                    "What will be the value of X in the following Python expression? \nX = 2+9*((3*12)-8)/10" ,
+                    "30.8",
+                    "27.2",
+                    "28.4",
+                    "30.0"
+                ],
+                [
+                    "Which of these in not a core data type?" ,
+                    "Tuples",
+                    "Dictionary",
+                    "Lists",
+                    "Class"
+                ],
+                [
+                    "Which of the following represents the bitwise XOR operator?" ,
+                    "&",
+                    "!",
+                    "^",
+                    "|"
+                ]
+            ]
+    answer = [
+                "[1, 2, ‘hello’]",
+                "34.000000",
+                "27.2",
+                "Class",
+                "^"
+             ]
+    li = ['',0,1,2,3,4]
+    x = random.choice(li[1:])
+    
+    ques = Label(easy_frame,text =easyQ[x][0],font="verdana 12",bg="white")
+    ques.place(relx=0.5,rely=0.2,anchor=CENTER)
+
+    var = StringVar()
+    
+    a = Radiobutton(easy_frame,text=easyQ[x][1],font="verdana 10",value=easyQ[x][1],variable = var,bg="white")
+    a.place(relx=0.5,rely=0.42,anchor=CENTER)
+
+    b = Radiobutton(easy_frame,text=easyQ[x][2],font="verdana 10",value=easyQ[x][2],variable = var,bg="white")
+    b.place(relx=0.5,rely=0.52,anchor=CENTER)
+
+    c = Radiobutton(easy_frame,text=easyQ[x][3],font="verdana 10",value=easyQ[x][3],variable = var,bg="white")
+    c.place(relx=0.5,rely=0.62,anchor=CENTER) 
+
+    d = Radiobutton(easy_frame,text=easyQ[x][4],font="verdana 10",value=easyQ[x][4],variable = var,bg="white")
+    d.place(relx=0.5,rely=0.72,anchor=CENTER) 
+    
+    li.remove(x)
+    
+    timer = Label(e)
+    timer.place(relx=0.8,rely=0.82,anchor=CENTER)
+    
+    
+    
+    def display():
+        
+        if len(li) == 1:
+                e.destroy()
+                showMark(score)
+        if len(li) == 2:
+            nextQuestion.configure(text='End',command=calc)
+                
+        if li:
+            x = random.choice(li[1:])
+            ques.configure(text =easyQ[x][0])
+            
+            a.configure(text=easyQ[x][1],value=easyQ[x][1])
+      
+            b.configure(text=easyQ[x][2],value=easyQ[x][2])
+      
+            c.configure(text=easyQ[x][3],value=easyQ[x][3])
+      
+            d.configure(text=easyQ[x][4],value=easyQ[x][4])
+            
+            li.remove(x)
+            print(li)
+            y = countDown()
+            if y == -1:
+                display()
+
+            
+    def calc():
+        global score
+        if (var.get() in answer):
+            score+=1
+        display()
+    
+    submit = Button(easy_frame,command=calc,text="Submit",width=20,font="verdana")
+    submit.place(relx=0.5,rely=0.82,anchor=CENTER)
+    
+    nextQuestion = Button(easy_frame,command=display,text="Next",font="verdana",width=5)
+    nextQuestion.place(relx=0.87,rely=0.82,anchor=CENTER)
+    
+    y = countDown()
+    if y == -1:
+        display()
+    e.mainloop()
+    
+    
+def medium():
+    menu.destroy()
+    global m
+    m = Tk()
+    
+    med_canvas = Canvas(m,width=720,height=440,bg="#101357")
+    med_canvas.pack()
+
+    med_frame = Frame(med_canvas,bg="white")
+    med_frame.place(relwidth=0.8,relheight=0.8,relx=0.1,rely=0.1)
+
+    
+    def countDown():
+        check = 0
+        for k in range(10, 0, -1):
+            
+            if k == 1:
+                check=-1
+            timer.configure(text=k)
+            med_frame.update()
+            time.sleep(1)
+            
+        timer.configure(text="Times up!")
+        if check==-1:
+            return (-1)
+        else:
+            return 0
+        
+    global score
+    score = 0
+    
+    mediumQ = [
+                [
+                    "Which of the following is not an exception handling keyword in Python?",
+                     "accept",
+                     "finally",
+                     "except",
+                     "try"
+                ],
+                [
+                    "Suppose list1 is [3, 5, 25, 1, 3], what is min(list1)?",
+                    "3",
+                    "5",
+                    "25",
+                    "1"
+                ],
+                [
+                    "Suppose list1 is [2, 33, 222, 14, 25], What is list1[-1]?",
+                    "Error",
+                    "None",
+                    "25",
+                    "2"
+                ],
+                [
+                    "print(0xA + 0xB + 0xC):",
+                    "0xA0xB0xC",
+                    "Error",
+                    "0x22",
+                    "33"
+                ],
+                [
+                    "Which of the following is invalid?",
+                    "_a = 1",
+                    "__a = 1",
+                    "__str__ = 1",
+                    "none of the mentioned"
+                ], 
+            ]
+    answer = [
+            "accept",
+            "1",
+            "25",
+            "33",
+            "none of the mentioned"
+            ]
+    
+    li = ['',0,1,2,3,4]
+    x = random.choice(li[1:])
+    
+    ques = Label(med_frame,text =mediumQ[x][0],font="verdana 12",bg="white")
+    ques.place(relx=0.5,rely=0.2,anchor=CENTER)
+
+    var = StringVar()
+    
+    a = Radiobutton(med_frame,text=mediumQ[x][1],font="verdana 10",value=mediumQ[x][1],variable = var,bg="white")
+    a.place(relx=0.5,rely=0.42,anchor=CENTER)
+
+    b = Radiobutton(med_frame,text=mediumQ[x][2],font="verdana 10",value=mediumQ[x][2],variable = var,bg="white")
+    b.place(relx=0.5,rely=0.52,anchor=CENTER)
+
+    c = Radiobutton(med_frame,text=mediumQ[x][3],font="verdana 10",value=mediumQ[x][3],variable = var,bg="white")
+    c.place(relx=0.5,rely=0.62,anchor=CENTER) 
+
+    d = Radiobutton(med_frame,text=mediumQ[x][4],font="verdana 10",value=mediumQ[x][4],variable = var,bg="white")
+    d.place(relx=0.5,rely=0.72,anchor=CENTER) 
+    
+    li.remove(x)
+    
+    timer = Label(m)
+    timer.place(relx=0.8,rely=0.82,anchor=CENTER)
+    
+    
+    
+    def display():
+        
+        if len(li) == 1:
+                m.destroy()
+                showMark(score)
+        if len(li) == 2:
+            nextQuestion.configure(text='End',command=calc)
+                
+        if li:
+            x = random.choice(li[1:])
+            ques.configure(text =mediumQ[x][0])
+            
+            a.configure(text=mediumQ[x][1],value=mediumQ[x][1])
+      
+            b.configure(text=mediumQ[x][2],value=mediumQ[x][2])
+      
+            c.configure(text=mediumQ[x][3],value=mediumQ[x][3])
+      
+            d.configure(text=mediumQ[x][4],value=mediumQ[x][4])
+            
+            li.remove(x)
+            print(li)
+            y = countDown()
+            if y == -1:
+                display()
+
+            
+    def calc():
+        global score
+        if (var.get() in answer):
+            score+=1
+        display()
+    
+    submit = Button(med_frame,command=calc,text="Submit",font="verdana",width=20)
+    submit.place(relx=0.5,rely=0.82,anchor=CENTER)
+    
+    nextQuestion = Button(med_frame,command=display,text="Next",font="verdana",width=5)
+    nextQuestion.place(relx=0.87,rely=0.82,anchor=CENTER)
+    
+    y = countDown()
+    if y == -1:
+        display()
+    m.mainloop()
+def difficult():
+    
+       
+    global h
+    h = Tk()
+    
+    hard_canvas = Canvas(h,width=720,height=440,bg="#101357")
+    hard_canvas.pack()
+
+    hard_frame = Frame(hard_canvas,bg="white")
+    hard_frame.place(relwidth=0.8,relheight=0.8,relx=0.1,rely=0.1)
+
+    
+    def countDown():
+        check = 0
+        for k in range(10, 0, -1):
+            
+            if k == 1:
+                check=-1
+            timer.configure(text=k)
+            hard_frame.update()
+            time.sleep(1)
+            
+        timer.configure(text="Times up!")
+        if check==-1:
+            return (-1)
+        else:
+            return 0
+        
+    global score
+    score = 0
+    
+    hardQ = [
+                [
+       "All keywords in Python are in _________",
+        "lower case",
+        "UPPER CASE",
+        "Capitalized",
+        "None of the mentioned"
+    ],
+    [
+        "Which of the following cannot be a variable?",
+        "__init__",
+        "in",
+        "it",
+        "on"
+    ],
+    [
+     "Which of the following is a Python tuple?",
+        "[1, 2, 3]",
+        "(1, 2, 3)",
+        "{1, 2, 3}",
+        "{}"   
+    ],
+    [
+        "What is returned by math.ceil(3.4)?",
+        "3",
+        "4",
+        "4.0",
+        "3.0"
+    ],
+    [
+        "What will be the output of print(math.factorial(4.5))?",
+        "24",
+        "120",
+        "error",
+        "24.0"
+    ] 
+            
+]
+    answer = [
+            "None of the mentioned",
+            "in",
+            "(1,2,3)",
+            "4",
+            "error"
+            ]
+    
+    li = ['',0,1,2,3,4]
+    x = random.choice(li[1:])
+    
+    ques = Label(hard_frame,text =hardQ[x][0],font="verdana 12",bg="white")
+    ques.place(relx=0.5,rely=0.2,anchor=CENTER)
+
+    var = StringVar()
+    
+    a = Radiobutton(hard_frame,text=hardQ[x][1],font="verdana 10",value=hardQ[x][1],variable = var,bg="white")
+    a.place(relx=0.5,rely=0.42,anchor=CENTER)
+
+    b = Radiobutton(hard_frame,text=hardQ[x][2],font="verdana 10",value=hardQ[x][2],variable = var,bg="white")
+    b.place(relx=0.5,rely=0.52,anchor=CENTER)
+
+    c = Radiobutton(hard_frame,text=hardQ[x][3],font="verdana 10",value=hardQ[x][3],variable = var,bg="white")
+    c.place(relx=0.5,rely=0.62,anchor=CENTER) 
+
+    d = Radiobutton(hard_frame,text=hardQ[x][4],font="verdana 10",value=hardQ[x][4],variable = var,bg="white")
+    d.place(relx=0.5,rely=0.72,anchor=CENTER) 
+    
+    li.remove(x)
+    
+    timer = Label(h)
+    timer.place(relx=0.8,rely=0.82,anchor=CENTER)
+    
+    
+    
+    def display():
+        
+        if len(li) == 1:
+                h.destroy()
+                showMark(score)
+        if len(li) == 2:
+            nextQuestion.configure(text='End',command=calc)
+                
+        if li:
+            x = random.choice(li[1:])
+            ques.configure(text =hardQ[x][0])
+            
+            a.configure(text=hardQ[x][1],value=hardQ[x][1])
+      
+            b.configure(text=hardQ[x][2],value=hardQ[x][2])
+      
+            c.configure(text=hardQ[x][3],value=hardQ[x][3])
+      
+            d.configure(text=hardQ[x][4],value=hardQ[x][4])
+            
+            li.remove(x)
+            print(li)
+            y = countDown()
+            if y == -1:
+                display()
+
+            
+    def calc():
+        global score
+        if (var.get() in answer):
+            score+=1
+        display()
+    
+    submit = Button(hard_frame,command=calc,text="Submit",font="verdana",width=20)
+    submit.place(relx=0.5,rely=0.82,anchor=CENTER)
+    
+    nextQuestion = Button(hard_frame,command=display,text="Next",font="verdana",width=5)
+    nextQuestion.place(relx=0.87,rely=0.82,anchor=CENTER)
+    
+    y = countDown()
+    if y == -1:
+        display()
+    h.mainloop()
+
+def showMark(mark):
+    global sh
+    sh = Tk()
+    
+    show_canvas = Canvas(sh,width=720,height=440,bg="#101357")
+    show_canvas.pack()
+
+    show_frame = Frame(show_canvas,bg="white")
+    show_frame.place(relwidth=0.8,relheight=0.8,relx=0.1,rely=0.1)
+    
+    st = "Your score is "+str(mark)
+    mlabel = Label(show_canvas,text=st,fg="black",font="verdana 20")
+    mlabel.place(relx=0.5,rely=0.2,anchor=CENTER)
+    
+    sh.mainloop()
+def start():
+    global root 
+    root = Tk()
+    canvas = Canvas(root,width = 720,height = 440, bg = 'yellow')
+    canvas.grid(column = 0 , row = 1)
+    img =PhotoImage(file="output-onlinepngtools.png")
+    canvas.create_image(50,10,image=img,anchor=NW)
+    #photo=ImageTk.PhotoImage(Image.open("output-onlinepngtools.png").resize((720,440),Image.ANTIALIAS))
+    #background_label=Label(canvas,image=photo)
+    #background_label.place(x=0,y=0)
+
+    button = Button(root, text='Start',command = menu) 
+    button.configure(width = 102,height=2, activebackground = "#33B5E5", relief = RAISED)
+    button.grid(column = 0 , row = 2)
+
+    root.mainloop()
+    
+    
+if __name__=='__main__':
+    start()
